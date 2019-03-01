5516a03bcc17f92f4a88f2af60feb1a6f3304060
raywu6 <rwu6@stuy.edu>
2019-03-01 13:17:51 -0500

    rename mango to mongo

--- 06_mongo/mongo.py
+++ 06_mongo/mongo.py
@@ -6,14 +6,14 @@
 import pymongo
 
 # connect to mongoDB-serving Simpson
-client = pymongo.MongoClient('lisa.stuy.edu')
+client = pymongo.MongoClient('159.89.179.201')
 
 # work with 'test' database
 db = client.test
 
 # test run on something that works...
 # docs = db.restaurants.find( {"address.zipcode" : "10282"} )
-# print( "========== TEST RUN ==========" )
+# print( "========== test run ==========" )
 # for doc in docs:
 #     print(doc)
 # print( "==============================" )
@@ -23,7 +23,7 @@ db = client.test
 def find_by_borough( borough ):
     borough = borough.capitalize() # necessary? social contract?
     docs = db.restaurants.find( {"borough":borough} )
-    print( "========== BOROUGH: {} ==========".format(borough) )
+    print( "========== borough: {} ==========".format(borough) )
     for doc in docs:
         print(doc)
     print( "==============================" )
@@ -33,7 +33,7 @@ def find_by_borough( borough ):
 def find_by_zip( zip_code ):
     zip_code = str(zip_code) # necessary? social contract?
     docs = db.restaurants.find( {"address.zipcode":zip_code} )
-    print( "========== ZIP CODE: {} ==========".format(zip_code) )
+    print( "========== zip code: {} ==========".format(zip_code) )
     for doc in docs:
         print(doc)
     print( "==============================" )
@@ -45,7 +45,7 @@ def find_by_zip_and_grade( zip_code , grade ):
     grade = str(grade)       # necessary? social contract?
     docs = db.restaurants.find( {'$and' : [{"address.zipcode":zip_code},
                                            {"grades.grade":grade}] } )
-    print( "========== ZIP CODE: {} , GRADE: {} ==========".format(zip_code,grade) )
+    print( "========== zip code: {} , grade: {} ==========".format(zip_code,grade) )
     for doc in docs:
         print(doc)
     print( "==============================" )    
@@ -57,7 +57,7 @@ def find_by_zip_below_grade( zip_code , grade ):
     grade = str(grade)       # necessary? social contract?
     docs = db.restaurants.find( {"$and" : [{"address.zipcode":zip_code},
                                            {"grades.score" : {'$lt':grade}} ]})
-    print( "========== ZIP CODE: {} , GRADE < {} ==========".format(zip_code,grade) )
+    print( "========== zip code: {} , grade < {} ==========".format(zip_code,grade) )
     for doc in docs:
         print(doc)
     print( "==============================" ) 
@@ -69,15 +69,15 @@ def find_by_zip_and_cuisine( zip_code , cuisine ):
     cuisine = cuisine.capitalize() # necessary? social contract?
     docs = db.restaurants.find( {"$and" : [{"address.zipcode":zip_code},
                                            {"cuisine" : cuisine} ]})
-    print( "========== ZIP CODE: {} , CUISINE: {} ==========".format(zip_code,cuisine) )
+    print( "========== zip code: {} , cuisine: {} ==========".format(zip_code,cuisine) )
     for doc in docs:
         print(doc)
     print( "==============================" ) 
     
-# ========== TEST CALLS ==========
-# find_by_borough( "Queens" )
+# ========== test calls ==========
+find_by_borough( "queens" )
 # find_by_borough( "queens" ) #should do same, no?
-# find_by_borough( "QUEENS" ) #should do same, no?
+# find_by_borough( "queens" ) #should do same, no?
 
 # find_by_zip( "10282" ) #should be same as test call before fxn defs
 # find_by_zip(  10282  ) #should do same, no?
@@ -85,8 +85,8 @@ def find_by_zip_and_cuisine( zip_code , cuisine ):
 # find_by_zip_and_grade( "10282", "30" )
 # find_by_zip_and_grade( 10282, 30 ) #should do same, no?
 
-find_by_zip_below_grade( "10282", "30" )
-find_by_zip_below_grade( 10282, 30 ) #should do same, no?
+# find_by_zip_below_grade( "10282", "30" )
+# find_by_zip_below_grade( 10282, 100 ) #should do same, no?
 
 # find_by_zip_and_cuisine( "10282", "Italian" )
 
